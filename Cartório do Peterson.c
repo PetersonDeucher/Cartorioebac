bfddc326fe154c7978d25375a6ae542b1d89911d
PetersonDeucher <petysilva688@gmail.com>
2024-11-15 20:35:11 +0000

    Opção de sair
    Foi incluso no sistema a opção de saída para o usuário

--- Cartório do Peterson.c
+++ Cartório do Peterson.c
@@ -125,6 +125,7 @@ int main()
 	  printf("\t1 - Registrar nomes\n");
 	  printf("\t2 - Consultar nomes\n");
 	  printf("\t3 - Deletar nomes\n\n"); 
+	  printf("\t4 - Sair do Sistema\n\n");
 	  printf("Opção: "); //\Fim do mnu
 	  
 	  scanf("%d", &opcao); //Armazenado a escolha do usuário
@@ -143,6 +144,11 @@ int main()
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por utilizar o sistema!\n");
+		return 0; //significa retorna ao valor 0, sair do sistema.
 		break;
 		
 		default:
